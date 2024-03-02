5ff3322742d95d7f16c133e926f7562aa9bee44e
Matt Dickson <matthew.dickson.137@gmail.com>
2024-03-02 00:31:43 -0800

    Add initial rules
    Adds rules for documents, sections, if expression, let expressions,
    error expressions, and try expressions.

--- grammar.js
+++ grammar.js
@@ -17,5 +17,65 @@ module.exports = grammar({
 	inline: $ => [],
 	precedences: $ => [],
 	conflicts: $ => [],
-	rules: {},
+	word: $ => $.identifier,
+	rules: {
+		document: $ => choice(repeat($.section), $.expression),
+		section: $ => seq(
+			optional($.literal_attributes),
+			"section",
+			$.identifier,
+			";",
+			repeat1($.section_member)
+		),
+		section_member: $ => seq(
+			optional($.literal_attributes),
+			optional("shared"),
+			$.identifier,
+			"=",
+			$.expression
+		),
+		expression: $ => choice(
+			$.let_expression,
+			$.if_expression
+		),
+
+		let_expression: $ => seq(
+			"let",
+			comma1(seq($.identifier, "=", $.expression)),
+			"in",
+			$.expression
+		),
+
+		if_expression: $ => seq(
+			"if",
+			$.expression,
+			"then",
+			$.expression,
+			"else",
+			$.expression
+		),
+
+		error_expression: $ => seq(
+			"error",
+			$.expression
+		),
+
+		try_expression: $ => seq(
+			"try",
+			$.expression,
+			optional(choice(
+				seq("otherwise", $.expression),
+				seq("catch", "(", optional($.identifier), ")", $.function_body)
+			))
+		),
+
+		identifier: _ => /@?[a-zA-Z][a-zA-Z0-9]/
 })
+
+function comma(rule) {
+	return optional(comma1(rule)) 
+}
+
+function comma1(rule) {
+	return seq(rule, repeat(seq(",", rule)))
+}
